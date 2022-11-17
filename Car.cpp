201d704c4a4a9569063850cfd91a38bc0e91afbc
dragosremetea <dragos.remetea@gmail.com>
2022-11-17 22:16:54 +0200

    commit first cpp class

---
+++ Car.cpp
@@ -0,0 +1,5 @@
+//
+// Created by drago on 17.11.2022.
+//
+
+#include "Car.h"
---
+++ Car.h
@@ -0,0 +1,14 @@
+//
+// Created by drago on 17.11.2022.
+//
+
+#ifndef PAOO_CAR_H
+#define PAOO_CAR_H
+
+
+class Car {
+
+};
+
+
+#endif //PAOO_CAR_H
